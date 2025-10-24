227a117654650f1bd63d52f078120d6ab979fc9d
Anas Alqoyyum <muhanasalqoyyum@gmail.com>
2025-10-24 10:53:34 +0700

    feat(style): change font to Atkinson and Google Sans Code

--- src/consts.ts
+++ src/consts.ts
@@ -1,8 +1,8 @@
 export const SITE_TITLE = 'Anas Alqoyyum'
-export const SITE_DESCRIPTION = "Hey, my name is Anas Alqoyyum. I'm a Software Engineer mostly focuses on Frontend Engineer in which primarily revolves around React and TypeScript."
+export const SITE_DESCRIPTION =
+  "Hey, my name is Anas Alqoyyum. I'm a Software Engineer mostly focuses on Frontend Engineer in which primarily revolves around React and TypeScript."
 export const SOCIAL_LINK = [
   ['GitHub', 'https://github.com/anasalqoyyum/'],
   ['LinkedIn', 'https://www.linkedin.com/in/anasalqoyyum/'],
   ['X (formerly Twitter)', 'https://www.twitter.com/sanaalqoyyum/']
 ]
-
